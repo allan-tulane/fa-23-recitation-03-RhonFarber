ff3171d6a38615ba7b237bb86c819e898a6bb35f
RhonFarber <122325331+RhonFarber@users.noreply.github.com>
2023-09-16 21:50:58 +0000

    implemented function

--- test_main.py
+++ test_main.py
@@ -1,7 +1,11 @@
 from main import *
 
 
-
 ## Feel free to add your own tests here.
 def test_multiply():
-    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+  assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2 * 2
+
+  # 9
+  assert quadratic_multiply(BinaryNumber(10), BinaryNumber(10)) == 10 * 10
+  assert quadratic_multiply(BinaryNumber(30), BinaryNumber(35)) == 30 * 35
+  assert quadratic_multiply(BinaryNumber(200), BinaryNumber(210)) == 200 * 210
